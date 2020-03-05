52f5b08fb415cde6cf4d8a1e16e574e9a59e22bd
mangoh <mangoh@mangoh.io>
2020-03-05 03:04:18 +0000

    Remove wifi

--- customSetupComp/customSetup.sh
+++ customSetupComp/customSetup.sh
@@ -8,18 +8,18 @@ source /etc/run.env
 case "$1" in
     start)
 	sleep 20
-	ifup wlan1
-	/legato/systems/current/bin/wifi ap setssid OptimusGearTest_AF
-	/legato/systems/current/bin/wifi ap setdriver 1
-	/legato/systems/current/bin/wifi ap setmaxclients 5
-	/legato/systems/current/bin/wifi ap setsecurityproto 1
-	/legato/systems/current/bin/wifi ap setpassphrase optimus1234
-	/legato/systems/current/bin/wifi ap setdiscoverable 1
-	/legato/systems/current/bin/wifi ap setchannel 11
-	/legato/systems/current/bin/wifi ap start wlan1
-	ifconfig wlan1 192.168.20.1 netmask 255.255.255.0 up
-	echo "dhcp-range=wlan1,192.168.20.10,192.168.20.100,24h" >> /etc/dnsmasq.d/dnsmasq.wlan.conf
-	/etc/init.d/dnsmasq start
+#	ifup wlan1
+#	/legato/systems/current/bin/wifi ap setssid OptimusGearTest_AF
+#	/legato/systems/current/bin/wifi ap setdriver 1
+#	/legato/systems/current/bin/wifi ap setmaxclients 5
+#	/legato/systems/current/bin/wifi ap setsecurityproto 1
+#	/legato/systems/current/bin/wifi ap setpassphrase optimus1234
+#	/legato/systems/current/bin/wifi ap setdiscoverable 1
+#	/legato/systems/current/bin/wifi ap setchannel 11
+#	/legato/systems/current/bin/wifi ap start wlan1
+#	ifconfig wlan1 192.168.20.1 netmask 255.255.255.0 up
+#	echo "dhcp-range=wlan1,192.168.20.10,192.168.20.100,24h" >> /etc/dnsmasq.d/dnsmasq.wlan.conf
+#	/etc/init.d/dnsmasq start
 	/legato/systems/current/bin/gnss start
 	mount -a
         ;;
