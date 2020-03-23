c115b38a31fece30a3ab300c96fdfb663fd1d1cf
mangoh <mangoh@mangoh.io>
2020-03-23 03:07:59 +0000

    chaned order of operations so that apps dont start without SDCARD. exit 1 when fails now.

--- customSetupComp/customSetup.sh
+++ customSetupComp/customSetup.sh
@@ -20,22 +20,7 @@ case "$1" in
 #	ifconfig wlan1 192.168.20.1 netmask 255.255.255.0 up
 #	echo "dhcp-range=wlan1,192.168.20.10,192.168.20.100,24h" >> /etc/dnsmasq.d/dnsmasq.wlan.conf
 #	/etc/init.d/dnsmasq start
-	/legato/systems/current/bin/app stop dataHub
-	/legato/systems/current/bin/app stop imu
-	/legato/systems/current/bin/gnss start
-	/legato/systems/current/bin/app start ogHeartbeat
-	
-	RESULT=$?
-	if [ $RESULT -ne 0 ]; then
-	echo 1 > /sys/devices/platform/expander.0/generic_led
-	else
-	echo 1 > /sys/devices/platform/expander.0/tri_led_red
-	echo 0 > /sys/devices/platform/expander.0/tri_led_grn
-	echo 0 > /sys/devices/platform/expander.0/tri_led_blue
-	echo 0 > /sys/devices/platform/expander.0/generic_led
-	fi
-	
-	sleep 5
+
 	
 	mkdir -p /mnt/userrw/sdcard
 	modprobe sdhci-msm
@@ -54,11 +39,28 @@ case "$1" in
 	echo 0 > /sys/devices/platform/expander.0/tri_led_grn
 	echo 0 > /sys/devices/platform/expander.0/tri_led_blue
 	echo 0 > /sys/devices/platform/expander.0/generic_led
+	exit 1
 	else
 	echo 0 > /sys/devices/platform/expander.0/tri_led_red
 	echo 1 > /sys/devices/platform/expander.0/generic_led
 	fi
-
+	
+	/legato/systems/current/bin/app stop dataHub
+	/legato/systems/current/bin/app stop imu
+	/legato/systems/current/bin/gnss start
+	/legato/systems/current/bin/app start ogHeartbeat
+	
+	RESULT=$?
+	if [ $RESULT -ne 0 ]; then
+	echo 1 > /sys/devices/platform/expander.0/generic_led
+	else
+	echo 1 > /sys/devices/platform/expander.0/tri_led_red
+	echo 0 > /sys/devices/platform/expander.0/tri_led_grn
+	echo 0 > /sys/devices/platform/expander.0/tri_led_blue
+	echo 0 > /sys/devices/platform/expander.0/generic_led
+	exit 1
+	fi
+	exit 0
         ;;
     monitor)
         ;;
